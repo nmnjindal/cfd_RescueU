b66cf585aefbde8d2a16e25971e2ec52b1dd1389
rohingupta10 <44088310+rohingupta10@users.noreply.github.com>
2018-10-26 16:52:42 +0530

    Removed apikey details

--- RescueU/settings.py
+++ RescueU/settings.py
@@ -126,8 +126,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.sendgrid.net'
 EMAIL_USE_TLS= True
 EMAIL_PORT=587
-EMAIL_HOST_USER='apikey'
-EMAIL_HOST_PASSWORD='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'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 
 LOGIN_REDIRECT_URL='users/sos/'
 LOGOUT_REDIRECT_URL='http://127.0.0.1:8000/volunteer/'
