e311a9620014977b247013141e5b98a548b0b65a
Rohin Gupta <rohingupta10@gmail.com>
2018-10-26 23:37:09 +0530

    User Interface upgraded

--- users/forms.py
+++ users/forms.py
@@ -10,3 +10,6 @@ class SignUpForm(UserCreationForm):
 	class Meta:
 		model=User
 		fields=('username','first_name','last_name','email','password1','password2')
+
+class commentform(forms.Form):
+	comment=forms.CharField(max_length=1000)
--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from volunteers.models import volunteer
 import geocoder
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from .forms import commentform
 # Create your views here.
 
 def home(request):
@@ -26,17 +27,23 @@ def hospitals(request):
 
 @login_required
 def sos(request):
-	subject='Help is needed at this location'
-	g=geocoder.ip('me')
-	message='Rescue as fast as possible\n'+'http://www.google.com/maps/place/'+str(g.latlng[0])+','+str(g.latlng[1])
-	email_from = settings.EMAIL_HOST_USER
-	recipient_list=[]
-	list=volunteer.objects.all()
-	#for entry in list:
-	#	recipient_list.append(entry.email)
-	recipient_list.append('naman@iitk.ac.in')
-	send_mail(subject,message,email_from,recipient_list)
-	return render(request,'users/sos.html')
+	if(request.method=='POST'):
+		comment=commentform(request.POST)
+		if(comment.is_valid()):
+			subject='Help is needed at this location'
+			g=geocoder.ip('me')
+			message='Rescue as fast as possible\n'+'http://www.google.com/maps/place/'+str(g.latlng[0])+','+str(g.latlng[1])+'\n'+comment.cleaned_data['comment']
+			email_from = settings.EMAIL_HOST_USER
+			recipient_list=[]
+			list=volunteer.objects.all()
+			for entry in list:
+				recipient_list.append(entry.email)
+			send_mail(subject,message,email_from,recipient_list)
+			return render(request,'users/sos_sent.html')
+	else:
+		form=commentform()
+		return render(request,'users/comment.html',{'form':form})
+
 
 
 
